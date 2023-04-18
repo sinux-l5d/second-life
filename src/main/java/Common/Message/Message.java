317a115ede90347de1ee3584457d988b0f7e26db
Simon LEONARD <git-1001af4@sinux.sh>
2023-04-18 17:46:37 +0100

    tweak Message.fromSerialized

--- src/main/java/Common/Message/Message.java
+++ src/main/java/Common/Message/Message.java
@@ -1,6 +1,7 @@
 package Common.Message;
 
 import Common.Side;
+import Server.Command.WrongParamsException;
 
 public abstract class Message {
     private final MessageType messageType;
@@ -8,6 +9,7 @@ public abstract class Message {
     /**
      * Creates a new message with the given type.
      * Intended to be used by subclasses.
+     *
      * @param messageType The type of the message.
      */
     protected Message(MessageType messageType) {
@@ -17,41 +19,66 @@ public abstract class Message {
     /**
      * Creates a new message from a serialized string.
      * Factory method.
+     *
      * @param serialized The serialized string.
      * @return The message.
      */
-    public static Message fromSerialized(String serialized) throws IllegalArgumentException {
+    public static Message fromSerialized(String serialized) throws IllegalArgumentException, WrongParamsException {
         String[] parts = serialized.split(":");
         MessageType messageType = MessageType.valueOf(parts[0]);
-        String[] params = parts[1].split(",");
+        String[] params = new String[0];
+        if (parts.length > 1) {
+            params = parts[1].split(",");
+        }
         switch (messageType) {
             case USER:
+                checkParams(params, 1);
                 return new UserMessage(params[0]);
             case ORDER:
+                checkParams(params, 3);
                 return new OrderMessage(Side.valueOf(params[0]), params[1], Double.parseDouble(params[2]));
             case CANCEL:
+                checkParams(params, 3);
                 return new CancelMessage(Side.valueOf(params[0]), params[1], Double.parseDouble(params[2]));
             case VIEW:
+                checkParams(params, 0);
                 return new ViewMessage();
             case END:
+                checkParams(params, 0);
                 return new EndMessage();
             case CONNECTED:
+                checkParams(params, 0);
                 return new ConnectedMessage();
             case MATCH:
+                checkParams(params, 4);
                 return new MatchMessage(Side.valueOf(params[0]), params[1], Double.parseDouble(params[2]), params[3]);
             case CANCELLED:
+                checkParams(params, 0);
                 return new CancelledMessage();
             case NOT_FOUND:
+                checkParams(params, 0);
                 return new NotFoundMessage();
             case ENDED:
+                checkParams(params, 0);
                 return new EndedMessage();
             default:
                 throw new IllegalArgumentException("Invalid message type: " + messageType);
         }
     }
 
+    private static void checkParams(String[] params, int expected) throws WrongParamsException {
+        if (params.length != expected)
+            throw new WrongParamsException("Invalid number of parameters, " + expected + " expected, " + params.length + " given");
+    }
+
     public MessageType getMessageType() {
         return messageType;
+    }
+
+    abstract public String toString();
+
+    protected String serialize(String[] params) {
+        return params.length == 0 ? messageType.toString() : messageType + ":" + String.join(",", params);
     }
 
     public enum MessageType {
@@ -66,11 +93,5 @@ public abstract class Message {
         CANCELLED,
         NOT_FOUND,
         ENDED
-    }
-
-    abstract public String toString();
-
-    protected String serialize(String[] params) {
-        return params.length == 0 ? messageType.toString() : messageType + ":" + String.join(",", params);
     }
 }
